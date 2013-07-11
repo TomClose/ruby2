9b01fb0093e131e5e87dbcb2abfa20e6e3d85eb0
Tom Close <tom.close@cantab.net>
2013-07-11 11:35:30 +0100

    Solution to bouncer.rb

--- bouncer.rb
+++ bouncer.rb
@@ -11,3 +11,25 @@
 #     Welcome to the club, Doris!
 
 # TODO: Write this program! (You have to be 18 to enter the club.)
+
+# ---- These functions are a bit unncessary, as we only use them once, but hey.
+def get_name
+    return gets.chomp.capitalize
+end
+
+def get_number
+    return gets.chomp.to_i
+end
+# ----
+
+print "What's your name? "
+name = get_name
+
+print "How old are you, #{name}? "
+age = get_number
+
+if age >= 18
+    puts "Welcome to the club, #{name}!"
+else
+    puts "I'm afraid I can't let you in, #{name}. You're too young!"
+end

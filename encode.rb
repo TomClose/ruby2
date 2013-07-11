98d417e7ec6669c0ee71fb0c79c648f6e7f637bb
Tom Close <tom.close@cantab.net>
2013-07-11 11:38:35 +0100

    Solutions to function_example and encode.rb

--- encode.rb
+++ encode.rb
@@ -1,10 +1,12 @@
 def encode(n)
 	# currently encodes by adding 6
 	# TODO change it so that it encodes by doing a bitwise_xor (^) with 6
-	return n + 6
+	return n ^ 6
 end
 
 
 def decode(n)
-	return n - 6
+    # This is a bit slick - it will work for encoding with any value, not just 6.
+    # Can you see why? (You might like to look at ruby1/explain.rb again.)
+	return n ^ encode(0)
 end
